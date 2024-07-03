d905b02b75c8158ec6d13e786ae69ef38e44d6cb
asif7480 <a.a.khan7480@gmail.com>
2024-07-03 21:22:24 +0500

    Added day 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,6 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
-import Dashboard from "./pages/user/Dashboard"
-import UserRoutes from "./Routes/UserRoutes"
-import GuestRoutes from "./Routes/GuestRoutes"
 function App() {
 
   return (
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,6 @@
 import { NavLink } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
 
 const Header = () => {
-  const { user, setUser } = useAuth()
-  const handleLogout = () => {
-    setUser({ email: "", orders: []})
-    localStorage.removeItem("email")
-    localStorage.removeItem("token")
-  }
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -43,36 +36,17 @@ const Header = () => {
                 </NavLink>
               </li>
 
-              {
-                user.email ? (
-                  <>
-                      <li className="nav-item">
-                        <NavLink to="/dashboard" className="nav-link">
-                          Dashboard
-                        </NavLink>
-                      </li>
-                    <li className="nav-item">
-                      <NavLink to="/login" onClick={handleLogout} className="nav-link">
-                        Logout
-                      </NavLink>
-                    </li>                  
-                  </>
-                ) : (
-                  <>
-                  <li className="nav-item">
-                    <NavLink to="/register" className="nav-link">
-                      Register
-                    </NavLink>
-                  </li>
+              <li className="nav-item">
+                <NavLink to="/register" className="nav-link">
+                  Register
+                </NavLink>
+              </li>
 
-                  <li className="nav-item">
-                    <NavLink to="/login" className="nav-link">
-                      Login
-                    </NavLink>
-                  </li>    
-                  </>
-                )
-              }
+              <li className="nav-item">
+                <NavLink to="/login" className="nav-link">
+                  Login
+                </NavLink>
+              </li>    
               
 
               <li className="nav-item">
